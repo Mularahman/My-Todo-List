5cb300d82907b174c1407340b97e24756848a905
mula <mularahman57@gmail.com>
2024-08-28 02:49:01 +0800

    update auth

---
+++ views/auth/login/schema.ts
@@ -0,0 +1,20 @@
+export const loginUserSchema = {
+    email: {
+        placeholder: "Enter user email",
+        validation: {
+            required: "Email is required!",
+            pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@gmail\.com$/,
+                message: "Email must be a valid @gmail.com address!",
+            },
+        },
+    
+    },
+    password: {
+        placeholder: "Enter user password",
+        validation: {
+            required: "Password is required!",
+        },
+    }
+
+}
