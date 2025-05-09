a01fb8a32cd3f760af61b633b70a8784eb45b56a
Mularahman <mularahman57@gmail.com>
2025-05-09 16:45:31 +0800

    run build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/My-Todo-List',
-  assetPrefix: '/My-Todo-List/',
+  // output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
